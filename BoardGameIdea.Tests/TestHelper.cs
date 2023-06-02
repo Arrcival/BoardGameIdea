2af6c99940553dc4a304243733c6f4142c419585
Arrcival <arrcival@gmail.com>
2023-06-02 20:12:28 +0200

    feat: commit first implementation

--- BoardGameIdea.Tests/TestHelper.cs
+++ BoardGameIdea.Tests/TestHelper.cs
@@ -7,9 +7,8 @@ public static class TestHelper
 {
     public static IGame SetupGame(int gameWidth, int playerHits, bool patternOverlaps, params PatternBase[] patterns)
     {
-        // Game game = new(gameWidth, playerHits, patternOverlaps);
-        // game.SetupPatterns(patterns);
-        // return game;
-        return null;
+        Game game = new(gameWidth, playerHits, patternOverlaps);
+        game.SetupPatterns(patterns);
+        return game;
     }
 }
