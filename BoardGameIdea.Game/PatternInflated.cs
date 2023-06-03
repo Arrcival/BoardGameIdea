24d2863956f20cb9a11192895be0c416f428a0f3
Arrcival <arrcival@gmail.com>
2023-06-04 01:19:12 +0200

    feat: third implementation but not working well

---
+++ BoardGameIdea.Game/PatternInflated.cs
@@ -0,0 +1,17 @@
+﻿namespace BoardGameIdea.Entities;
+
+public class PatternInflated
+{
+    public (int, int)[] Coordinates;
+    public int Score;
+
+    public PatternInflated((int, int)[] coordinates, int x, int y, int score)
+    {
+        Coordinates = new (int, int)[coordinates.Length];
+        for (int i = 0; i < coordinates.Length; i++)
+        {
+            Coordinates[i] = new(coordinates[i].Item1 + x, coordinates[i].Item2 += y);
+        }
+        Score = score;
+    }
+}
